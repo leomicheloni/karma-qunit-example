0021f8ee3385fb1896322d5e5cb408decb201339
Leonardo <=>
2014-12-22 23:43:11 -0300

    add function implemented

--- js/calculator.js
+++ js/calculator.js
@@ -2,6 +2,10 @@ Calculator = (function(){
 	var Calculator = function(){
 	};
 	
+	Calculator.prototype.add = function(a, b){
+		return a + b;
+	};
+	
 	return Calculator;
 
 }());
--- test/calculatortest.js
+++ test/calculatortest.js
@@ -5,3 +5,9 @@ var calculator = new Calculator();
 test("Calculator should not be undefined", function(assert){
 	assert.ok( calculator != "undefined");
 });
+
+
+test("simple add", function(assert){
+	var result = calculator.add(5, 3);
+	equal(result, 8);
+});
